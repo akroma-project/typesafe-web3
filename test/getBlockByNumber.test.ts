a3309c2c92d326159c7d3002abeb86ac49eb6c83
detroitpro <eric@polerecky.com>
2022-01-30 11:52:15 -0500

    chore: code clean/lint/rename

--- test/getBlockByNumber.test.ts
+++ test/getBlockByNumber.test.ts
@@ -7,7 +7,7 @@ const sut = new TypeSafeWeb3();
 describe("get block by number", () => {
   it("should get block by number", async () => {
     const result = await sut.getBlockByNumber(1661071, true);
-    // print(result);
+
     expect(result.data?.transactions).toHaveLength(2);
   });
 
